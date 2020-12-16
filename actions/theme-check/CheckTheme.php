4de1d70a43bcc0fe49cd54ea70a7a418f9bf024f
dufresnesteven <steve.dufresne@automattic.com>
2020-12-16 10:58:55 +0900

    Try php spacing.

--- actions/theme-check/CheckTheme.php
+++ actions/theme-check/CheckTheme.php
@@ -68,7 +68,7 @@ class WPORG_CheckTheme {
 	public function print_message( $type, $messages ) {
 		echo '::' . esc_attr( $type ) . '::';
 
-		$eol = '%0A';
+		$eol = PHP_EOL;
 
 		foreach ( $messages as $key => $val ) {
 			$implode = implode( $eol, $val );
