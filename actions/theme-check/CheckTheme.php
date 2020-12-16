10ccfb03c089c0a52a861d9fb98c0c9158bd57fe
dufresnesteven <steve.dufresne@automattic.com>
2020-12-16 10:55:08 +0900

    Force github spacing.

--- actions/theme-check/CheckTheme.php
+++ actions/theme-check/CheckTheme.php
@@ -68,7 +68,7 @@ class WPORG_CheckTheme {
 	public function print_message( $type, $messages ) {
 		echo '::' . esc_attr( $type ) . '::';
 
-		$eol = ( ! defined( 'DEV_MODE' ) ? '%0A' : PHP_EOL );
+		$eol = '%0A';
 
 		foreach ( $messages as $key => $val ) {
 			$implode = implode( $eol, $val );
